e4736d3e4faa030b0c17c2bbf0e207884ad880f7
Tuan Tran Van <hunter.tran.92@outlook.com>
2019-09-13 16:23:43 -0400

    scrap with level

--- wordcloud/spiders/paho_spider.py
+++ wordcloud/spiders/paho_spider.py
@@ -3,11 +3,6 @@ import scrapy
 
 class PahoSpider(scrapy.Spider):
     name = "paho"
-    baseUrl = ""
-
-    def extractBaseUrl(self, url):
-        fragments = url.split('/')
-        return fragments[0] + "//" + fragments[2]
 
     def start_requests(self):
         filePath = './wordcloud/spiders/sitelist.txt'
@@ -16,29 +11,55 @@ class PahoSpider(scrapy.Spider):
         
         index = 0
         for line in siteDataList:
-            isScrapped = line.split(',')[0]
-            url = line.split(',')[1]
+            siteData = line.split(',')
+            isScrapped = siteData[0]
+            scrapLevel = siteData[1]
+            url = siteData[2]
             if isScrapped == '0':
-                siteDataList[index] = '1,' + url + '\n'
+                # TODO: edit here to stop re-scraping a site on different run
+                siteDataList[index] = '0,' + scrapLevel + ',' + url + '\n'
                 baseUrl = self.extractBaseUrl(url)
                 request = scrapy.Request(url,callback=self.parseRootUrl)
                 request.cb_kwargs['root'] = baseUrl
+                # TODO: convert scrapLevel to number
+                request.cb_kwargs['level'] = int(scrapLevel)
+                request.cb_kwargs['current_level'] = 0
                 yield request
             index += 1
         
         with open(filePath, 'w') as dataFile:
             dataFile.writelines(siteDataList)
 
-    def parseRootUrl(self, response, root):
+    def extractBaseUrl(self, url):
+        fragments = url.split('/')
+        return fragments[0] + "//" + fragments[2]
+
+    def parseRootUrl(self, response, root,level, current_level):
         urls = response.selector.xpath("//a/@href").getall()
 
         for url in urls:
             if url.startswith('#') == False:
-                if url.startswith('http'):
-                    self.log("end")
-                    yield scrapy.Request(url, callback=self.parseUrl)
-                else:
-                    yield scrapy.Request(url=root + url, callback=self.parseUrl)
+                urlToScrap = self.buildUrl(root, url)
+
+                request = self.buildRequest(root,urlToScrap,level,current_level)
+
+                yield request
+
+    def buildRequest(self,root, url, level, currentLevel):
+        if level == currentLevel:
+            request = scrapy.Request(url, callback=self.parseUrl)
+        else:
+            request = scrapy.Request(url, callback=self.parseRootUrl)
+            request.cb_kwargs['root'] = root
+            request.cb_kwargs['level'] = level
+            request.cb_kwargs['current_level'] = currentLevel +1
+        return request
+
+    def buildUrl(self, root, url):
+        if url.startswith('http'):
+            return url
+        else:
+            return root + url
 
     def cleanText(self, text):
         text = text.strip()
