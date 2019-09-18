4edd08e94f8e53d26ca703a0f19ae8dba25d52ac
Tuan Tran Van <hunter.tran.92@outlook.com>
2019-09-18 14:42:47 -0400

    generate file of words with freq

--- nlp/preprocessing.py
+++ nlp/preprocessing.py
@@ -2,12 +2,18 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.stem import SnowballStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk import FreqDist
+import os
+import collections
 
 
 def loadRawDataFile():
     with open('./wordcloud/data/paho.txt', 'r', encoding="utf-8") as dataFile:
         return dataFile.readlines()
 
+def createDataFolder():
+    if not os.path.exists('./nlp/data/'):
+        os.makedirs('./nlp/data/')
 
 def combineStopwords():
     customizedStopwords = []
@@ -30,6 +36,18 @@ def combineStopwords():
 
 
         keywords.extend([word for word in words if word not in combinedStopwords and word != '' and '//' not in word])
+
+    fd = FreqDist(keywords)
+    sortedFreqDist = sorted(fd.items(), key=lambda kv:kv[1], reverse=True)
+
+    createDataFolder()
+    with open('./nlp/data/paho.txt','w+',encoding='utf-8') as resultFile:
+        for key, value in sortedFreqDist:
+            resultFile.writelines(key + ':' + str(value) + '\n')
+        # resultFile.write(','.join(fd.items()))
+
+    # print(fd.most_common(100))
+    # fd.plot()
     
-    keywords.sort()
-    print(keywords)
+    # keywords.sort()
+    # print(keywords)
