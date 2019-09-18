cc3533463ce29aae78612d5b87acbbe836ba9d1a
Tuan Tran Van <hunter.tran.92@outlook.com>
2019-09-18 16:11:19 -0400

    preprocessing and sort by weight

---
+++ nlp/model/keyword.py
@@ -0,0 +1,5 @@
+class Keyword(object):
+    def __init__(self, word=None, frequency=None, tag=None):
+        self.word = word
+        self.frequency = frequency
+        self.tag = tag
--- nlp/preprocessing.py
+++ nlp/preprocessing.py
@@ -3,8 +3,12 @@ from nltk.stem import PorterStemmer
 from nltk.stem import SnowballStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk import FreqDist
+from nltk.probability import ConditionalFreqDist
+from nltk import pos_tag
 import os
 import collections
+from nltk.stem import WordNetLemmatizer
+from nlp.model.keyword import Keyword
 
 
 def loadRawDataFile():
@@ -15,8 +19,20 @@ def createDataFolder():
     if not os.path.exists('./nlp/data/'):
         os.makedirs('./nlp/data/')
 
+def getProgrammingLanguageList():
+    programmingLanguages = []
+    with open('./nlp/programming_languages.txt','r',encoding='utf-8') as programmingLanguagesFile:
+        lines = programmingLanguagesFile.readlines()
+        for line in lines:
+            programmingLanguages.append(line.strip('\n'))
+
+    return programmingLanguages
+
 def combineStopwords():
+    programmingLanguages = getProgrammingLanguageList()
+
     customizedStopwords = []
+
     with open('./nlp/customized_stopwords.txt', 'r', encoding='utf-8') as customizedStopwordsFile:
         customizedStopwords = customizedStopwordsFile.readlines()
         for index, word in enumerate(customizedStopwords):
@@ -27,6 +43,7 @@ def combineStopwords():
 
     dataLines = loadRawDataFile()
     keywords = []
+    programmingLanguageKeywords = []
     for line in dataLines:
         words = word_tokenize(line)
 
@@ -34,15 +51,40 @@ def combineStopwords():
             # words[index] = SnowballStemmer('english').stem(word)
             words[index] = PorterStemmer().stem(word)
 
+            # check for stop words
+            if words[index] not in combinedStopwords and words[index] != '' and '//' not in words[index]:
+                if words[index] in programmingLanguages:
+                    programmingLanguageKeywords.append(words[index])
+                else:
+                    keywords.append(words[index])
 
-        keywords.extend([word for word in words if word not in combinedStopwords and word != '' and '//' not in word])
+
+        # keywords.extend([word for word in words if word not in combinedStopwords and word != '' and '//' not in word])
+    # lemmatizer = WordNetLemmatizer()
+    # with open('./nlp/programming_language_lemma.txt','r',encoding='utf-8') as programmingLanguageLemmaFile:
+    #     lines = programmingLanguageLemmaFile.readlines()
+    #     for line in lines:
+    #         lemmatizer.lemmatize(line.strip('\n'),'pl')
+    
+    # programmingLanguageKeywords = lemmatizer.lemmatize()
 
     fd = FreqDist(keywords)
+    plfd = FreqDist(programmingLanguageKeywords)
+
+    # result = []
+    # for word in fd.items():
+    #     result.append(Keyword(word[0], word[1]))
+
     sortedFreqDist = sorted(fd.items(), key=lambda kv:kv[1], reverse=True)
+    sortedProgrammingLanguagesFreqDist = sorted(plfd.items(), key=lambda kv:kv[1], reverse=True)
 
     createDataFolder()
     with open('./nlp/data/paho.txt','w+',encoding='utf-8') as resultFile:
         for key, value in sortedFreqDist:
+            resultFile.writelines(key + ':' + str(value) + '\n')
+
+    with open('./nlp/data/paho-programmingLanguages.txt','w+',encoding='utf-8') as resultFile:
+        for key, value in sortedProgrammingLanguagesFreqDist:
             resultFile.writelines(key + ':' + str(value) + '\n')
         # resultFile.write(','.join(fd.items()))
 
