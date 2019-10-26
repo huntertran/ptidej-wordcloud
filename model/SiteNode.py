dd95214f02965ac2a839e1998fc7a090e8cf7796
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2019-10-26 16:09:04 -0400

    display the graph

--- model/SiteNode.py
+++ model/SiteNode.py
@@ -1,4 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
 class SiteNode(object):
+    index: int
+    key: str
+
     def __init__(self, index=0, key=None):
         self.index = index
         self.key = key
--- nlp/Linker.py
+++ nlp/Linker.py
@@ -8,8 +8,28 @@ defaultEncoding = 'utf-8'
 resultedDataPath = './data/nlp/result/'
 
 
+def print_graph(relationships, siteNodes):
+    # print headers
+    print('', end='\t', flush=True)
+    for node in siteNodes:
+        print(node.key, end='\t', flush=True)
+    print('\n')
+
+    index = 0
+    for row in relationships:
+        print(siteNodes[index].key, end='\t', flush=True)
+
+        for col in row:
+            print(col, end='\t', flush=True)
+        
+        print('\n')
+        index += 1
+
+
+
 def create_link():
     projectNodes = []
+    relationships = []
 
     filePath = './data/sitelist.json'
 
@@ -20,7 +40,7 @@ def create_link():
     for site in siteDataList:
         siteName = ProjectHelper.getProjectName(site.SiteUrl)
         # remove the 'iot'
-        siteName = siteName.replace('iot','').replace('.',' ').strip()
+        siteName = siteName.replace('iot', '').replace('.', ' ').strip()
 
         # handle special case
         if('agile' in siteName):
@@ -28,8 +48,17 @@ def create_link():
 
         siteNode = SiteNode(index, siteName)
 
+        # TODO: REMOVE AFTER DEBUG
+        if(index > 5):
+            break
+
         projectNodes.append(siteNode)
         index += 1
 
     for siteNode in projectNodes:
         print(siteNode.key)
+        relationships.append([0] * len(projectNodes))
+
+    print("End creating connected graph")
+
+    print_graph(relationships, projectNodes)
