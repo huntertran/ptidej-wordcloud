ec104b8870e236ab3d22e27ccbf2a4cd3bc6918c
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-09-30 15:57:23 +0000

    analyze with pattern

--- nlp/SentenceAnalyzer.py
+++ nlp/SentenceAnalyzer.py
@@ -3,14 +3,16 @@
 import json
 from model.LinkKeyword import LinkKeyword, LinkKeywordEncoder, LinkProject
 from types import SimpleNamespace as Namespace
-from nltk import pos_tag
+from nltk import pos_tag, RegexpParser
 
 linked_keywords = []
+
 
 def load_linked_result():
     file_path = './data/linked.json'
     with open(file_path, 'r') as dataFile:
         return json.load(dataFile, object_hook=lambda d: Namespace(**d))
+
 
 def start_analyze():
     linked_keywords = load_linked_result()
@@ -19,4 +21,7 @@ def start_analyze():
             for sentence in project.Sentences:
                 words = sentence.split()
                 tokens = pos_tag(words)
-                print(tokens)
+                grammar = "NP: {<VB>?<NNP>?}"
+                cp = RegexpParser(grammar)
+                result = cp.parse(tokens)
+                print(result)
