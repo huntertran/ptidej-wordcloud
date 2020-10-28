3bc36af7aabb96e3aab0ab8f211e0c1820335f8a
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-10-28 16:27:10 +0000

    modulize javascripts

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -1,148 +1,161 @@
-// custom node renderer
+var wordcloud = {
+    data: 'data/graph.json',
+    s: undefined,
 
-sigma.utils.pkg('sigma.canvas.nodes');
-sigma.canvas.nodes.image = (function () {
-    var _cache = {},
-        _loading = {},
-        _callbacks = {};
+    customRender: function () {
+        // custom node renderer
 
-    // Return the renderer itself:
-    var renderer = function (node, context, settings) {
-        var args = arguments,
-            prefix = settings('prefix') || '',
-            size = node[prefix + 'size'],
-            color = node.color || settings('defaultNodeColor'),
-            url = node.image.url;
-            w = node.image.w;
-            h = node.image.h;
-            scale = 0.1;
+        sigma.utils.pkg('sigma.canvas.nodes');
+        sigma.canvas.nodes.image = (function () {
+            var _cache = {},
+                _loading = {},
+                _callbacks = {};
 
-        if (_cache[url]) {
-            context.save();
+            // Return the renderer itself:
+            var renderer = function (node, context, settings) {
+                var args = arguments,
+                    prefix = settings('prefix') || '',
+                    size = node[prefix + 'size'],
+                    color = node.color || settings('defaultNodeColor'),
+                    url = node.image.url;
+                w = node.image.w;
+                h = node.image.h;
+                scale = 0.1;
 
-            // // Draw the clipping disc:
-            // context.beginPath();
-            // context.arc(
-            //     node[prefix + 'x'],
-            //     node[prefix + 'y'],
-            //     node[prefix + 'size'],
-            //     0,
-            //     Math.PI * 2,
-            //     true
-            // );
-            // context.closePath();
-            // context.clip();
+                if (_cache[url]) {
+                    context.save();
 
-            // Draw the image
-            context.drawImage(
-                _cache[url],
-                node[prefix + 'x'] - size,
-                node[prefix + 'y'] - size,
-                w * scale * size,
-                h * scale * size
-            );
+                    // // Draw the clipping disc:
+                    // context.beginPath();
+                    // context.arc(
+                    //     node[prefix + 'x'],
+                    //     node[prefix + 'y'],
+                    //     node[prefix + 'size'],
+                    //     0,
+                    //     Math.PI * 2,
+                    //     true
+                    // );
+                    // context.closePath();
+                    // context.clip();
 
-            // console.log("draw image: W" + w + " | H: " + h + " | Size: " + size)
+                    // Draw the image
+                    context.drawImage(
+                        _cache[url],
+                        node[prefix + 'x'] - size,
+                        node[prefix + 'y'] - size,
+                        w * scale * size,
+                        h * scale * size
+                    );
 
-            // // Quit the "clipping mode":
-            // context.restore();
+                    // console.log("draw image: W" + w + " | H: " + h + " | Size: " + size)
 
-            // // Draw the border:
-            // context.beginPath();
-            // context.arc(
-            //     node[prefix + 'x'],
-            //     node[prefix + 'y'],
-            //     node[prefix + 'size'],
-            //     0,
-            //     Math.PI * 2,
-            //     true
-            // );
-            // context.lineWidth = size / 5;
-            // context.strokeStyle = node.color || settings('defaultNodeColor');
-            // context.stroke();
-        } else {
-            sigma.canvas.nodes.image.cache(url);
-            sigma.canvas.nodes.def.apply(
-                sigma.canvas.nodes,
-                args
-            );
-        }
-    };
+                    // // Quit the "clipping mode":
+                    // context.restore();
 
-    // Let's add a public method to cache images, to make it possible to
-    // preload images before the initial rendering:
-    renderer.cache = function (url, callback) {
-        if (callback)
-            _callbacks[url] = callback;
+                    // // Draw the border:
+                    // context.beginPath();
+                    // context.arc(
+                    //     node[prefix + 'x'],
+                    //     node[prefix + 'y'],
+                    //     node[prefix + 'size'],
+                    //     0,
+                    //     Math.PI * 2,
+                    //     true
+                    // );
+                    // context.lineWidth = size / 5;
+                    // context.strokeStyle = node.color || settings('defaultNodeColor');
+                    // context.stroke();
+                } else {
+                    sigma.canvas.nodes.image.cache(url);
+                    sigma.canvas.nodes.def.apply(
+                        sigma.canvas.nodes,
+                        args
+                    );
+                }
+            };
 
-        if (_loading[url])
-            return;
+            // Let's add a public method to cache images, to make it possible to
+            // preload images before the initial rendering:
+            renderer.cache = function (url, callback) {
+                if (callback)
+                    _callbacks[url] = callback;
 
-        var img = new Image();
+                if (_loading[url])
+                    return;
 
-        img.onload = function () {
-            _loading[url] = false;
-            _cache[url] = img;
+                var img = new Image();
 
-            if (_callbacks[url]) {
-                _callbacks[url].call(this, img);
-                delete _callbacks[url];
-            }
-        };
+                img.onload = function () {
+                    _loading[url] = false;
+                    _cache[url] = img;
 
-        _loading[url] = true;
-        img.src = url;
-    };
+                    if (_callbacks[url]) {
+                        _callbacks[url].call(this, img);
+                        delete _callbacks[url];
+                    }
+                };
 
-    return renderer;
-})();
+                _loading[url] = true;
+                img.src = url;
+            };
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function config() {
-    // s.refresh();
-
-    // sigma.plugins.relativeSize(s, 5);
-
-    s.startForceAtlas2({
-        worker: true,
-        barnesHutOptimize: false,
-        scalingRatio: 1
-    });
-    await sleep(3000);
-    s.killForceAtlas2();
-
-    var noOverLapListener = s.configNoverlap({
-        nodeMargin: 3.0,
-        scaleNodes: 1.3
-    });
-    s.startNoverlap();
-
-    var dragListener = new sigma.plugins.dragNodes(s, s.renderers[0]);
-    // CustomShapes.init(s);
-    // s.refresh();
-}
-
-var s = new sigma({
-    renderer: {
-        container: document.getElementById('container'),
-        type: 'canvas'
+            return renderer;
+        })();
     },
-    settings: {
-        minArrowSize: 10,
-        enableEdgeHovering: true,
-        edgeHoverSizeRatio: 2,
-        zoomMin: 0.001
-        // defaultEdgeColor: "#ff0000"
-    }
-});
 
-sigma.parsers.json(
-    'data/graph.json',
-    s,
-    async function () {
-        await config();
+    sleep: function (ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    },
+
+    config: async function () {
+        // s.refresh();
+
+        // sigma.plugins.relativeSize(s, 5);
+
+        wordcloud.s.startForceAtlas2({
+            worker: true,
+            barnesHutOptimize: false,
+            scalingRatio: 1
+        });
+        await wordcloud.sleep(3000);
+        wordcloud.s.killForceAtlas2();
+
+        var noOverLapListener = wordcloud.s.configNoverlap({
+            nodeMargin: 3.0,
+            scaleNodes: 1.3
+        });
+        wordcloud.s.startNoverlap();
+
+        var dragListener = new sigma.plugins.dragNodes(wordcloud.s, wordcloud.s.renderers[0]);
+        // CustomShapes.init(s);
+        // s.refresh();
+    },
+
+    init: function () {
+        wordcloud.customRender();
+
+        wordcloud.s = new sigma({
+            renderer: {
+                container: document.getElementById('container'),
+                type: 'canvas'
+            },
+            settings: {
+                minArrowSize: 10,
+                enableEdgeHovering: true,
+                edgeHoverSizeRatio: 2,
+                zoomMin: 0.001
+                // defaultEdgeColor: "#ff0000"
+            }
+        });
+
+        sigma.parsers.json(
+            wordcloud.data,
+            wordcloud.s,
+            async function () {
+                await wordcloud.config();
+            }
+        );
     }
-);
+}
+
+wordcloud.init();
