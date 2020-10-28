98a2c28a7d1d3f19dd2cd3f38960e48cb3759a66
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2020-10-28 16:49:52 +0000

    split functions for clean code

--- docs/scripts/scripts.js
+++ docs/scripts/scripts.js
@@ -1,105 +1,107 @@
 var wordcloud = {
     data: 'data/graph.json',
     s: undefined,
+    rendererArgs: {
+        _cache: {},
+        _loading: {},
+        _callbacks: {}
+    },
+
+    renderer: function (node, context, settings) {
+        var args = arguments,
+            prefix = settings('prefix') || '',
+            size = node[prefix + 'size'],
+            color = node.color || settings('defaultNodeColor'),
+            url = node.image.url;
+        w = node.image.w;
+        h = node.image.h;
+        scale = 0.1;
+
+        if (wordcloud.rendererArgs._cache[url]) {
+            context.save();
+
+            // // Draw the clipping disc:
+            // context.beginPath();
+            // context.arc(
+            //     node[prefix + 'x'],
+            //     node[prefix + 'y'],
+            //     node[prefix + 'size'],
+            //     0,
+            //     Math.PI * 2,
+            //     true
+            // );
+            // context.closePath();
+            // context.clip();
+
+            // Draw the image
+            context.drawImage(
+                wordcloud.rendererArgs._cache[url],
+                node[prefix + 'x'] - size,
+                node[prefix + 'y'] - size,
+                w * scale * size,
+                h * scale * size
+            );
+
+            // console.log("draw image: W" + w + " | H: " + h + " | Size: " + size)
+
+            // // Quit the "clipping mode":
+            // context.restore();
+
+            // // Draw the border:
+            // context.beginPath();
+            // context.arc(
+            //     node[prefix + 'x'],
+            //     node[prefix + 'y'],
+            //     node[prefix + 'size'],
+            //     0,
+            //     Math.PI * 2,
+            //     true
+            // );
+            // context.lineWidth = size / 5;
+            // context.strokeStyle = node.color || settings('defaultNodeColor');
+            // context.stroke();
+        } else {
+            sigma.canvas.nodes.image.cache(url);
+            sigma.canvas.nodes.def.apply(
+                sigma.canvas.nodes,
+                args
+            );
+        }
+    },
+
+    rendererCache: function (url, callback) {
+        if (callback)
+            wordcloud.rendererArgs._callbacks[url] = callback;
+
+        if (wordcloud.rendererArgs._loading[url])
+            return;
+
+        var img = new Image();
+
+        img.onload = function () {
+            wordcloud.rendererArgs._loading[url] = false;
+            wordcloud.rendererArgs._cache[url] = img;
+
+            if (wordcloud.rendererArgs._callbacks[url]) {
+                wordcloud.rendererArgs._callbacks[url].call(this, img);
+                delete wordcloud.rendererArgs._callbacks[url];
+            }
+        };
+
+        wordcloud.rendererArgs._loading[url] = true;
+        img.src = url;
+    },
 
     customRender: function () {
         // custom node renderer
 
         sigma.utils.pkg('sigma.canvas.nodes');
         sigma.canvas.nodes.image = (function () {
-            var _cache = {},
-                _loading = {},
-                _callbacks = {};
-
-            // Return the renderer itself:
-            var renderer = function (node, context, settings) {
-                var args = arguments,
-                    prefix = settings('prefix') || '',
-                    size = node[prefix + 'size'],
-                    color = node.color || settings('defaultNodeColor'),
-                    url = node.image.url;
-                w = node.image.w;
-                h = node.image.h;
-                scale = 0.1;
-
-                if (_cache[url]) {
-                    context.save();
-
-                    // // Draw the clipping disc:
-                    // context.beginPath();
-                    // context.arc(
-                    //     node[prefix + 'x'],
-                    //     node[prefix + 'y'],
-                    //     node[prefix + 'size'],
-                    //     0,
-                    //     Math.PI * 2,
-                    //     true
-                    // );
-                    // context.closePath();
-                    // context.clip();
-
-                    // Draw the image
-                    context.drawImage(
-                        _cache[url],
-                        node[prefix + 'x'] - size,
-                        node[prefix + 'y'] - size,
-                        w * scale * size,
-                        h * scale * size
-                    );
-
-                    // console.log("draw image: W" + w + " | H: " + h + " | Size: " + size)
-
-                    // // Quit the "clipping mode":
-                    // context.restore();
-
-                    // // Draw the border:
-                    // context.beginPath();
-                    // context.arc(
-                    //     node[prefix + 'x'],
-                    //     node[prefix + 'y'],
-                    //     node[prefix + 'size'],
-                    //     0,
-                    //     Math.PI * 2,
-                    //     true
-                    // );
-                    // context.lineWidth = size / 5;
-                    // context.strokeStyle = node.color || settings('defaultNodeColor');
-                    // context.stroke();
-                } else {
-                    sigma.canvas.nodes.image.cache(url);
-                    sigma.canvas.nodes.def.apply(
-                        sigma.canvas.nodes,
-                        args
-                    );
-                }
-            };
-
             // Let's add a public method to cache images, to make it possible to
             // preload images before the initial rendering:
-            renderer.cache = function (url, callback) {
-                if (callback)
-                    _callbacks[url] = callback;
+            wordcloud.renderer.cache = wordcloud.rendererCache;
 
-                if (_loading[url])
-                    return;
-
-                var img = new Image();
-
-                img.onload = function () {
-                    _loading[url] = false;
-                    _cache[url] = img;
-
-                    if (_callbacks[url]) {
-                        _callbacks[url].call(this, img);
-                        delete _callbacks[url];
-                    }
-                };
-
-                _loading[url] = true;
-                img.src = url;
-            };
-
-            return renderer;
+            return wordcloud.renderer;
         })();
     },
 
